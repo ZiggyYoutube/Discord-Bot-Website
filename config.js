ea6f01e101dd3ba3d1967b843f06814634368ad9
ZiggyYoutube <149648930+ZiggyYoutube@users.noreply.github.com>
2023-11-02 00:33:42 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { MdAddModerator } from "react-icons/md";
 import { SiProbot } from "react-icons/si";
 import { TbMessage2Code } from "react-icons/tb";
 const config = {
-  name: "ProBot",
+  name: "Rythem",
   //BotName
   inviteLink:
     "https://discord.com/oauth2/authorize?client_id=879602399067660338&permissions=8&scope=bot&redirect_uri=https://docs.markos.ga&response_type=code",
@@ -59,7 +59,7 @@ const config = {
   mediumLink: "https://medium.com/",
   facebookLink: "https://facebook.com",
   discordLink: "https://discord.com",
-  youtubeLink: "https://www.youtube.com/channel/UCfhRHeex9lU0ZAVJot6Z2ZQ",
+  youtubeLink: "https://www.youtube.com",
 };
 
 export default config;
